a2acffb21d2cec9faa546a469a5744bb21921be2
drizdva-kul <dmytro.rizdvanetskyi@student.kuleuven.be>
2025-10-15 11:10:44 +0200

    Added comment pointing to original source

--- methyldl/modelling/minirnns/helpers.py
+++ methyldl/modelling/minirnns/helpers.py
@@ -1,10 +1,4 @@
-# Copyright (c) 2025-present, Royal Bank of Canada.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-
+# https://github.com/lucidrains/minGRU-pytorch/blob/main/minGRU_pytorch/minGRU.py
 import torch
 import torch.nn.functional as F
 
